7eda2f8524cb5cbdd4734fc255f7656491b2ce48
Saravanajd <saravana.jd@gmail.com>
2018-08-24 20:51:40 +0530

    First Commit
    YearPicker - A customer picker for select year only

---
+++ yearpicker.js
@@ -0,0 +1,10 @@
+(function($,document,Date){
+    'use strict';
+
+    var _setupError = 'YearPicker Error';
+    if(!$ || !$.moment){
+        alert('The jQuery and moment.js plugin must be loaded!!');
+        return;
+    }
+
+});
