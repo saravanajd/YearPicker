a0e52b2e25357f47d7cc082b313e942e395c2d02
Saravanajd <saravana.jd@gmail.com>
2018-08-27 20:11:01 +0530

    assign a selected date to textbox

--- yearpicker.js
+++ yearpicker.js
@@ -1,10 +1,47 @@
-(function($,document,Date){
+(function ($) {
     'use strict';
 
-    var _setupError = 'YearPicker Error';
-    if(!$ || !$.moment){
-        alert('The jQuery and moment.js plugin must be loaded!!');
-        return;
+    // var _setupError = 'YearPicker Error';
+    // if (!($) || !($.moment)) {
+    //     alert('The jQuery and moment.js plugin must be loaded!!');
+    //     return;
+    // }
+
+    $.fn.yearpicker = function (options) {
+        var defaults = {
+            minDate: "1900",
+            maxDate: "",
+            date: "",
+        };
+        var $settings = $.extend({}, defaults, options)
+
+        var $currentYear = new Date().getFullYear();
+        var $yearpicker = $(this);
+
+        debug($yearpicker);
+        log($yearpicker);
+        log($currentYear);
+        $yearpicker.val($currentYear);
+
+        $('.yearpicker-year .yearpicker-items').on('click', function () {
+            var selectedDate = $(this).html();
+            $yearpicker.val(selectedDate);
+        })
+    }
+    var debug = (obj) => {
+        if (window.console && window.console.log) {
+            window.console.log("yearpicker counts " + obj.length);
+        }
     }
 
-});
+    var log = (obj) => {
+        if (window.console && window.console.log) {
+            window.console.log(obj);
+        }
+    }
+
+
+
+}(jQuery));
+
+$('.yearpicker').yearpicker();
